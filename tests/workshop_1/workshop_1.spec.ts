5bd9c667b4460371a6a740c2488dc4e708efec55
Ravindu Karunaweera <ravindu.karunaweera@ifs.com>
2024-04-29 16:58:07 +0530

    tried to solve the timeout issue

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -1,7 +1,16 @@
 import {test} from "playwright/test";
 
 test('Basic Navigation', async ({page}) => {
-    await page.goto('https://gitlab.com/');
+    const navigationTimeout = 60000; //60 seconds
+
+    await page.goto('https://gitlab.com/', {timeout: navigationTimeout, waitUntil:'load'});
     await page.waitForTimeout(3000);
     await page.reload();
+})
+
+test.only ('Interacting with Web Elements on GitLab', async ({page}) => {
+    const navigationTimeout = 90000; //90 seconds
+
+    await page.goto('https://gitlab.com/', {timeout: navigationTimeout, waitUntil:'load'});
+    await page.click('#slp-btn intro-block__button slp-mr-24 slp-btn-primary');
 })
